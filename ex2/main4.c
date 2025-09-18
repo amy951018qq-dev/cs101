331fe5e4de21dce669f364d6ba03789c448c5c09
asriel2006 <amy951018qq@gmail.com>
2025-09-18 15:45:59 +0800

    Create main4.c

---
+++ ex2/main4.c
@@ -0,0 +1,11 @@
+int main(){
+    int a = 10;
+    
+    if(a == 10){
+        printf("a is equal to 10\n");
+    }else if(a<20){
+        printf("a is less than 20\n");
+    }else {
+        printf("a is not than 20\n");
+    }
+}
